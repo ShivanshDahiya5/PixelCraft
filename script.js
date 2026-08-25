c4276615c72870c4186071f9e0f614ef018b5391
ShivanshDahiya5 <shivanshdahiya563@gmail.com>
2026-08-25 12:15:41 +0530

    Add mouse event handling for crop box resizing functionality

--- script.js
+++ script.js
@@ -71,3 +71,11 @@ const applyState = (s) => {
 
 let activeDragging = false, activeResizing = false, currentHandle = "";
 let startX, startY, startW, startH, startL, startT;
+
+cropBox.addEventListener("mousedown", (e) => {
+    startX = e.clientX; startY = e.clientY;
+    startW = cropBox.offsetWidth; startH = cropBox.offsetHeight;
+    startL = cropBox.offsetLeft; startT = cropBox.offsetTop;
+    
+    if(e.target.classList.contains("handle")) {
+        activeResizing = true;
